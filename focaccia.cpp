b92746015296e07126c850f1d2b42a2b02577a2d
cwjng <97417286+cwjng@users.noreply.github.com>
2022-05-09 22:10:10 -0400

    Add files via upload

---
+++ focaccia.cpp
@@ -0,0 +1,58 @@
+/**
+ * focaccia.cpp
+ *
+ * <#Name#>
+ * <#Uniqname#>
+ *
+ * EECS 183: Project 1
+ * Winter 2022
+ * 
+ * Project UID: fde244392017fe65ebcc34f01c226b11f113bb3dc6dae8af4cb6ea11bf76f7c8
+ */
+
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+/**
+ * Returns the singular or plural form of a word, based on number
+ *
+ * Requires: singular is the singular form of the word.
+ *           plural is the plural form of the word.
+ *           number determines how many things there are; must be >= 0.
+ * Modifies: Nothing.
+ * Effects:  Returns return the singular form of the word if number == 1.
+ *           Otherwise, returns the plural form.
+ * Examples:
+ *           // prints "bag"
+ *           cout << pluralize("bag", "bags", 1);
+ *
+ *           // prints "pounds"
+ *           string temp = pluralize("pound", "pounds", 3);
+ *           cout << temp;
+ */
+string pluralize(string singular, string plural, int number);
+
+
+
+int main() {
+    int people;
+    cout << "How many people do you need to serve? ";
+    cin >> people;
+    cout << endl << endl;
+
+    // TODO: add your code below
+
+    return 0;
+}
+
+// ----------------------------------------------
+// *** DO NOT CHANGE ANYTHING BELOW THIS LINE ***
+
+string pluralize(string singular, string plural, int number) {
+    if (number == 1) {
+        return singular;
+    }
+    return plural;
+}
